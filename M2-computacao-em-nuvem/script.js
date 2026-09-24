b4174932d3af3a5e9bc02a6debe0b7a66eaa9675
cadufelix010 <carlos2019felix@gmail.com>
2026-09-24 10:47:21 -0300

    Remove pasta antiga M2-computacao-em-nuvem

--- M2-computacao-em-nuvem/script.js
+++
@@ -1,9 +0,0 @@
-function responder(correta) {
-  const resultado = document.getElementById('resultado');
-
-  if (correta) {
-    resultado.textContent = 'Correto! SaaS significa Software como Serviço.';
-  } else {
-    resultado.textContent = 'Resposta incorreta. Tente novamente.';
-  }
-}
